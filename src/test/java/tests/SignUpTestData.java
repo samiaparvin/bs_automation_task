d2fbfafd7a2ea60d9d189cbc44faab756229fba9
Samia parvin <samiaparvin33@gmail.com>
2022-02-21 00:02:37 +0600

    Project init

---
+++ src/test/java/tests/SignUpTestData.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class SignUpTestData<User1email> {
+
+    public String[] User1 = {"Ani", "Sam", "123456", "Ani", "Sam", "Cole Dillard LLC", "91 Rocky Milton Road",
+    "Quia sit mollit dolo", "Illo maxime exceptur", "39170", "Commodo aut commodo", "+1 (626) 177-8533", "+1 (254) 164-2201", "Cillum commodi commo"};
+
+    public String[] User2 = {"Chaim", "Sykes", "123456", "Chaim", "Sykes", "Cole Dillard LLC", "91 Rocky Milton Road",
+            "Quia sit mollit dolo", "Illo maxime exceptur", "39170", "Commodo aut commodo", "+1 (626) 177-8533", "+1 (254) 164-2201", "Cillum commodi commo"};
+
+    public String[] Useremails = {"samiatest1@email.com", "samiatest1@email.com" };
+
+}
